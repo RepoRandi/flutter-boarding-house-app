8323c80dc8a62f0e15ebca9319cfd0d3173e7398
Randi Maulana Akbar <randimaulanaakbar@gmail.com>
2021-07-08 17:58:24 +0700

    First commit

---
+++ android/app/src/main/kotlin/com/example/boarding_house/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.boarding_house
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text('Boarding House'),
+          ),
+          body: Center(
+            child: Text('Boarding House'),
+          )),
+    );
+  }
+}

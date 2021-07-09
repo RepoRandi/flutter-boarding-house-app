7d787c521eb9de88111a01c5219db00c7b4e065b
Randi Maulana Akbar <randimaulanaakbar@gmail.com>
2021-07-09 19:57:44 +0700

    Add Text Style

--- lib/themes/theme.dart
+++ lib/themes/theme.dart
@@ -1,7 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 Color purpleColor = Color(0xff5843BE);
 Color orangeColor = Color(0xffFF9376);
 Color blackColor = Color(0xff000000);
 Color whiteColor = Color(0xffFFFFFF);
-Color grayColor = Color(0xff82868E);
+Color greyColor = Color(0xff82868E);
+
+TextStyle blackTextStyle = GoogleFonts.poppins(
+  fontWeight: FontWeight.w500,
+  color: blackColor,
+);
+
+TextStyle whiteTextStyle = GoogleFonts.poppins(
+  fontWeight: FontWeight.w500,
+  color: whiteColor,
+);
+
+TextStyle greyTextStyle = GoogleFonts.poppins(
+  fontWeight: FontWeight.w300,
+  color: greyColor,
+);
+
+TextStyle purpleTextStyle = GoogleFonts.poppins(
+  fontWeight: FontWeight.w500,
+  color: purpleColor,
+);

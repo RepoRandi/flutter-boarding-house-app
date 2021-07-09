aa09b87f3eb77dab34ea19a844053af1879d0d3a
Randi Maulana Akbar <randimaulanaakbar@gmail.com>
2021-07-09 20:56:23 +0700

    Refactor themes

--- lib/themes/theme.dart
+++ lib/themes/theme.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 Color purpleColor = Color(0xff5843BE);
 Color orangeColor = Color(0xffFF9376);
@@ -7,22 +6,28 @@ Color blackColor = Color(0xff000000);
 Color whiteColor = Color(0xffFFFFFF);
 Color greyColor = Color(0xff82868E);
 
-TextStyle blackTextStyle = GoogleFonts.poppins(
+TextStyle blackTextStyle = TextStyle(
   fontWeight: FontWeight.w500,
   color: blackColor,
 );
 
-TextStyle whiteTextStyle = GoogleFonts.poppins(
+TextStyle blackRegularTextStyle = TextStyle(
+  fontSize: 16,
+  fontWeight: FontWeight.w400,
+  color: blackColor,
+);
+
+TextStyle whiteTextStyle = TextStyle(
   fontWeight: FontWeight.w500,
   color: whiteColor,
 );
 
-TextStyle greyTextStyle = GoogleFonts.poppins(
+TextStyle greyTextStyle = TextStyle(
   fontWeight: FontWeight.w300,
   color: greyColor,
 );
 
-TextStyle purpleTextStyle = GoogleFonts.poppins(
+TextStyle purpleTextStyle = TextStyle(
   fontWeight: FontWeight.w500,
   color: purpleColor,
 );

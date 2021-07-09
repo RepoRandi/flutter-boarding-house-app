c714a0d5d5835553a2a0773caaec2da5eacba21a
Randi Maulana Akbar <randimaulanaakbar@gmail.com>
2021-07-09 13:34:12 +0700

    Add themes & dependencies google fonts

---
+++ lib/themes/theme.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+Color purpleColor = Color(0xff5843BE);
+Color orangeColor = Color(0xffFF9376);
+Color blackColor = Color(0xff000000);
+Color whiteColor = Color(0xffFFFFFF);
+Color grayColor = Color(0xff82868E);
